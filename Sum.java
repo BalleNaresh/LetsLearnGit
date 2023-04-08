74ac20a2ec6a5dc773cc3f69907a768f682029a9
Naresh Balle <ballenaresh456@gmail.com>
2023-04-08 20:35:23 +0530

    added Sum.java after modification

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum
 {
     public static void main(String args [])
     {
-        system.out.println("welcome to the world of programming");
+        system.out.println("welcome to the world of programming by naresh");
     }
 }
